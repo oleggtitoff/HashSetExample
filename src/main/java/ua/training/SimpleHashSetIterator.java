7483aa74fe2b2a07c60e0380e62daf1a0b27ae33
Oleg Titov <oleggtitoff@gmail.com>
2019-12-19 13:23:25 +0200

    Added SimpleHashSetIterator class that implements Iterator.

---
+++ src/main/java/ua/training/SimpleHashSetIterator.java
@@ -0,0 +1,15 @@
+package ua.training;
+
+import java.util.Iterator;
+
+public class SimpleHashSetIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
