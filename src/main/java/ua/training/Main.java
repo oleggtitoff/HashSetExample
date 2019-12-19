8ec84be728487079af9141eac6938e3eaf63eb85
Oleg Titov <oleggtitoff@gmail.com>
2019-12-19 14:19:51 +0200

    In Main added example output.

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,9 +1,27 @@
 package ua.training;
 
+import ua.training.model.entity.SimpleHashSet;
+import ua.training.model.entity.SimpleSet;
+
 public class Main {
 
     public static void main(String[] args) {
+        SimpleSet<String> set = new SimpleHashSet<>(128);
 
+        set.add("first");
+        set.add("second");
+        set.add("third");
+
+        System.out.println(set.size());
+        set.iterator().forEachRemaining(System.out::println);
+
+        set.remove("second");
+
+        System.out.println(set.size());
+        set.iterator().forEachRemaining(System.out::println);
+
+        System.out.println(set.contains("third"));
+        System.out.println(set.contains("second"));
     }
 
 }
